816e2b6a25ed32e15d42109fff1ad1267db13376
Donald Silveira <donaldsilveira@gmail.com>
2024-05-11 20:41:15 -0300

    Fix search bar margin

--- src/components/composed/search-bar.tsx
+++ src/components/composed/search-bar.tsx
@@ -10,7 +10,7 @@ export function SearchBar() {
   const [query, setQuery] = useQueryState("search");
 
   return (
-    <div className="relative pt-4">
+    <div className="relative mt-4">
       <Search className="absolute w-6 h-6 left-4 top-2/4 -translate-y-2/4 text-zinc-400" />
       <Input
         className="pr-14 pl-14"
