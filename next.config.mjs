3460a8840892441c72ed9e2a076c23041869e6fd
Donald Silveira <donaldsilveira@gmail.com>
2024-05-10 23:13:09 -0300

    Base next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
+import "./src/env.mjs";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
 export default nextConfig;
