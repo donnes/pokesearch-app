05e354524f2625839c680a82936c96f47a3767db
Donald Silveira <donaldsilveira@gmail.com>
2024-05-11 02:45:39 -0300

    Created SearchBar component

---
+++ src/components/composed/search-bar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Search, X } from "lucide-react";
+import { useQueryState } from "nuqs";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export function SearchBar() {
+  const [query, setQuery] = useQueryState("search");
+
+  return (
+    <div className="sticky top-6">
+      <div className="relative mt-4">
+        <Search className="absolute w-6 h-6 left-4 top-2/4 -translate-y-2/4 text-zinc-400" />
+        <Input
+          className="pr-14 pl-14"
+          placeholder="Search"
+          value={query || ""}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        {query && (
+          <Button
+            type="button"
+            className="absolute right-2 top-2/4 -translate-y-2/4"
+            size="icon"
+            variant="secondary"
+            onClick={() => setQuery(null)}
+          >
+            <X className="w-6 h-6 text-zinc-400" />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-14 w-full rounded-2xl border px-3 py-2 file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 border-zinc-800 bg-zinc-950 ring-offset-zinc-950 placeholder:text-zinc-400 focus-visible:ring-zinc-300",
+          "flex h-14 w-full rounded-2xl border px-3 py-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 border-zinc-800 bg-zinc-950 ring-offset-zinc-950 placeholder:text-zinc-400 focus-visible:ring-zinc-300",
           className,
         )}
         ref={ref}
