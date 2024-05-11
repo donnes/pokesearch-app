0ba822c3ff31919d370c9ba63750b432101c920a
Donald Silveira <donaldsilveira@gmail.com>
2024-05-11 20:39:58 -0300

    Created favorite store

---
+++ src/hooks/use-favorite-store.ts
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+import type { NamedAPIResource } from "@/schemas/shared";
+
+interface FavoriteState {
+  favorites: Array<NamedAPIResource>;
+  saveFavorite: (resource: NamedAPIResource) => void;
+  removeFavorite: (name: string) => void;
+}
+
+export const useFavoriteStore = create<FavoriteState>()(
+  persist(
+    (set, get) => ({
+      favorites: [],
+      saveFavorite(newPokemon) {
+        const pokemon = get().favorites.find(
+          (pokemon) => pokemon.name === newPokemon.name
+        );
+
+        if (!pokemon) return;
+
+        return set((state) => ({
+          favorites: [newPokemon, ...state.favorites],
+        }));
+      },
+      removeFavorite(name) {
+        return set((state) => ({
+          favorites: state.favorites.filter((pokemon) => pokemon.name !== name),
+        }));
+      },
+    }),
+    {
+      name: "favorites-storage",
+    }
+  )
+);
