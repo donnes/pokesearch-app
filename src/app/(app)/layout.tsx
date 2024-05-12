37bf2ae200d031a85102da2276e772667a004a08
Donald Silveira <donaldsilveira@gmail.com>
2024-05-11 21:09:41 -0300

    Refactor routes

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,22 @@
 import type { ReactNode } from "react";
-import { Providers } from "./providers";
+
+import { DotPattern } from "@/components/patterns/dot-pattern";
+import { cn } from "@/lib/utils";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <Providers>{children}</Providers>;
+  return (
+    <main className="flex min-h-screen flex-col max-w-3xl py-12 md:py-24 px-6 md:px-0 mx-auto">
+      <DotPattern
+        width={20}
+        height={20}
+        cx={1}
+        cy={1}
+        cr={1}
+        className={cn(
+          "fixed [mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)]",
+        )}
+      />
+      {children}
+    </main>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 import { Toaster } from "@/components/ui/sonner";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <Providers>{children}</Providers>
         <Toaster />
       </body>
     </html>
