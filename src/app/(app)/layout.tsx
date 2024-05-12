badb93ab109d1ba3d41b5258a302a4064dac245b
Donald Silveira <donaldsilveira@gmail.com>
2024-05-12 03:16:20 -0300

    Fix project build

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,9 +1,9 @@
-import type { ReactNode } from "react";
+import * as React from "react";
 
 import { DotPattern } from "@/components/patterns/dot-pattern";
 import { cn } from "@/lib/utils";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex min-h-screen flex-col max-w-3xl py-12 md:py-24 px-6 md:px-0 mx-auto">
       <DotPattern
@@ -16,7 +16,7 @@ export default function Layout({ children }: { children: ReactNode }) {
           "fixed [mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)]",
         )}
       />
-      {children}
+      <React.Suspense>{children}</React.Suspense>
     </main>
   );
 }
