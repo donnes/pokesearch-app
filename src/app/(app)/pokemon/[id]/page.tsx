a84c7f872a4c0fcd190286f8198285084f51f693
Donald Silveira <donaldsilveira@gmail.com>
2024-05-12 00:54:41 -0300

    Display pokemon stats

--- src/app/(app)/pokemon/[id]/page.tsx
+++ src/app/(app)/pokemon/[id]/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
 import {
-  BoltIcon,
   ChevronLeft,
-  HeartIcon,
   RulerIcon,
-  ShieldIcon,
   StarIcon,
   StarOffIcon,
-  SwordIcon,
   WeightIcon,
 } from "lucide-react";
 import Image from "next/image";
@@ -17,8 +13,10 @@ import { notFound, useParams } from "next/navigation";
 import * as React from "react";
 
 import { PokeballLoading } from "@/components/composed/pokeball-loading";
+import { StatsIcon } from "@/components/composed/stats-icon";
 import { Badge, type BadgeProps } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Progress, type ProgressProps } from "@/components/ui/progress";
 import { env } from "@/env.mjs";
 import { useToggleFavoriteMutation } from "@/hooks/use-mutations";
 import { useGetFavoriteQuery, useGetPokemonQuery } from "@/hooks/use-queries";
@@ -123,47 +121,6 @@ export default function PokemonPage() {
             <div className="grid grid-cols-2 gap-4 pt-4">
               <div>
                 <h3 className="font-medium">
-                  <HeartIcon className="mr-2 inline-block h-5 w-5" />
-                  HP
-                </h3>
-                <p className="text-sm text-gray-400 pt-2">
-                  {pokemon.stats.find((stat) => stat.stat.name === "hp")
-                    ?.base_stat || 0}
-                </p>
-              </div>
-              <div>
-                <h3 className="font-medium">
-                  <SwordIcon className="mr-2 inline-block h-5 w-5" />
-                  Attack
-                </h3>
-                <p className="text-sm text-gray-400 pt-2">
-                  {pokemon.stats.find((stat) => stat.stat.name === "attack")
-                    ?.base_stat || 0}
-                </p>
-              </div>
-              <div>
-                <h3 className="font-medium">
-                  <ShieldIcon className="mr-2 inline-block h-5 w-5" />
-                  Defense
-                </h3>
-                <p className="text-sm text-gray-400 pt-2">
-                  {pokemon.stats.find((stat) => stat.stat.name === "defense")
-                    ?.base_stat || 0}
-                </p>
-              </div>
-              <div>
-                <h3 className="font-medium">
-                  <BoltIcon className="mr-2 inline-block h-5 w-5" />
-                  Special Attack
-                </h3>
-                <p className="text-sm text-gray-400 pt-2">
-                  {pokemon.stats.find(
-                    (stat) => stat.stat.name === "special-attack",
-                  )?.base_stat || 0}
-                </p>
-              </div>
-              <div>
-                <h3 className="font-medium">
                   <WeightIcon className="mr-2 inline-block h-5 w-5" />
                   Weight
                 </h3>
@@ -180,38 +137,70 @@ export default function PokemonPage() {
                   {pokemon.height / 10} m
                 </p>
               </div>
-            </div>
-          </div>
 
-          <div>
-            <h2 className="text-2xl font-medium">Abilities</h2>
-            <div className="grid grid-cols-2 gap-4 pt-2">
-              {pokemon.abilities
-                .filter(({ ability }) => ability.name)
-                .map(({ ability }) => (
-                  <div
-                    key={ability.name}
-                    className="capitalize px-3 py-2 rounded-xl bg-zinc-800"
-                  >
-                    {ability.name.replace(/-/g, " ")}
+              <div className="col-span-2 h-px bg-zinc-800 my-2" />
+
+              {pokemon.stats
+                .filter(({ stat }) => stat.name)
+                .map(({ stat, base_stat }) => (
+                  <div key={stat.name}>
+                    <h3
+                      className={cn("font-medium capitalize", {
+                        uppercase: stat.name.length === 2,
+                      })}
+                    >
+                      <StatsIcon stat={stat.name} />
+                      {stat.name.replace(/-/g, " ")}
+                    </h3>
+                    <p className="text-sm text-gray-400 pt-2">{base_stat}</p>
                   </div>
                 ))}
             </div>
           </div>
+
+          {pokemon.abilities.length > 0 && (
+            <div>
+              <h2 className="text-2xl font-medium">Abilities</h2>
+              <div className="grid grid-cols-2 gap-4 pt-2">
+                {pokemon.abilities
+                  .filter(({ ability }) => ability.name)
+                  .map(({ ability }) => (
+                    <div
+                      key={ability.name}
+                      className="capitalize px-3 py-2 rounded-xl bg-zinc-800"
+                    >
+                      {ability.name.replace(/-/g, " ")}
+                    </div>
+                  ))}
+              </div>
+            </div>
+          )}
 
           <div>
             <h2 className="text-2xl font-medium">Stats</h2>
             <div className="grid grid-cols-2 gap-4 pt-2">
-              {pokemon.abilities
-                .filter(({ ability }) => ability.name)
-                .map(({ ability }) => (
-                  <div
-                    key={ability.name}
-                    className="capitalize px-3 py-2 rounded-xl bg-zinc-800"
-                  >
-                    {ability.name.replace(/-/g, " ")}
-                  </div>
-                ))}
+              {pokemon.stats
+                .filter(({ stat }) => stat.name)
+                .map(({ stat, base_stat }) => {
+                  return (
+                    <div key={stat.name}>
+                      <h3
+                        className={cn("capitalize text-sm text-zinc-400 pb-2", {
+                          uppercase: stat.name.length === 2,
+                        })}
+                      >
+                        <StatsIcon stat={stat.name} className="h-4 w-4" />
+                        {stat.name.replace(/-/g, " ")}
+                      </h3>
+
+                      <Progress
+                        value={(base_stat / 300) * 100}
+                        max={100}
+                        variant={stat.name as keyof ProgressProps["variant"]}
+                      />
+                    </div>
+                  );
+                })}
             </div>
           </div>
         </div>
