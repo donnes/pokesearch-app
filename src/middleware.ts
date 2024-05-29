5ad45eef934d01a4c636a4ad7e77614940f62cb0
Donald Silveira <donaldsilveira@gmail.com>
2024-05-29 18:06:30 -0300

    Improved middleware to handle public and private routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,29 @@ import { updateSession } from "@/lib/supabase/middleware";
 import { type NextRequest, NextResponse } from "next/server";
 import { createClient } from "./lib/supabase/server";
 
+const publicRoutes = ["/", "/signin"];
+
 export async function middleware(request: NextRequest) {
   const response = await updateSession(request);
   const supabase = createClient();
+
+  const url = new URL("/", request.url);
   const nextUrl = request.nextUrl;
 
   const { data } = await supabase.auth.getUser();
 
-  // Not authenticated
-  if (!data.user && !nextUrl.pathname.includes("/signin")) {
+  // Not authenticated, public routes
+  if (!data.user && publicRoutes.includes(nextUrl.pathname)) {
     return NextResponse.next();
+  }
+
+  // Not authenticated, private routes
+  if (!data.user) {
+    const encodedSearchParams = `${url.pathname.substring(1)}${url.search}`;
+    const newUrl = new URL("/signin", url);
+    newUrl.searchParams.append("return_to", encodedSearchParams);
+
+    return NextResponse.redirect(newUrl);
   }
 
   // Authenticated
